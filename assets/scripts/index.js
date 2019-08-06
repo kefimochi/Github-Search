7b4c2d38c0294d7f00e21a581240f47ccde578bd
Kate Efimova <kate.efimova0@gmail.com>
2019-08-06 12:48:27 -0700

    Added fetch, now  stores recieved search data

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,29 @@
-import GitHub from "github-api";
-const Octokit = require("@octokit/rest");
-const octokit = new Octokit();
+// import GitHub from "github-api";
+// const Octokit = require("@octokit/rest");
+// const octokit = new Octokit();
+
+(function() {
+  let form = document.querySelector("#form");
+
+  form.addEventListener("submit", function(e) {
+    let input = document.getElementById("search-bar").value;
+    console.log("Concole log", input);
+    e.preventDefault(); // Stop page refresh.
+
+    let data = new URLSearchParams(new FormData(form));
+
+    let url = `https://api.github.com/search/users?q=${input}`;
+
+    fetch(url, {
+      method: "get"
+    }).then(res => {
+      //   console.log(res.json());
+      renameLater(res.json());
+      return res.json();
+    });
+
+    function renameLater(users) {
+      console.log(users);
+    }
+  });
+})();
