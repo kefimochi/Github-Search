6c66893cbd33fc52091d8b6dae9fbdb44d039da7
Kate Efimova <kate.efimova0@gmail.com>
2019-08-06 13:00:48 -0700

    Added comments, deleted unnecessary npm packages

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,25 +1,21 @@
-// import GitHub from "github-api";
-// const Octokit = require("@octokit/rest");
-// const octokit = new Octokit();
-
 (function() {
   let form = document.querySelector("#form");
 
   form.addEventListener("submit", function(e) {
+    // Captures a string entered in the search field.
     let input = document.getElementById("search-bar").value;
-    console.log("Concole log", input);
-    e.preventDefault(); // Stop page refresh.
-
-    let data = new URLSearchParams(new FormData(form));
-
+    // Adds searched value to GitHub's API
     let url = `https://api.github.com/search/users?q=${input}`;
+    // Prevents page from reloading and sending data to a sever.
+    e.preventDefault();
 
+    // Goes to a provided url and converts the received data into JSON object
     fetch(url, {
       method: "get"
     }).then(res => {
-      //   console.log(res.json());
-      renameLater(res.json());
-      return res.json();
+      res.json().then(result => {
+        console.log(result.items);
+      });
     });
 
     function renameLater(users) {
