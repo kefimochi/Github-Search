3d6f5758193203aa7274d0f65821480350ecbd9e
Kate Efimova <kate.efimova0@gmail.com>
2019-08-06 17:06:29 -0700

    In process of figuring out how to get detail data

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -8,7 +8,7 @@
     let url = `https://api.github.com/search/users?q=${input}`;
     // Selects a container that shows a brief users desciption and
     // a total amount of users found.
-    let list = document.querySelector(".all-users-container");
+    let userList = document.querySelector(".all-users-container");
     // Selects a page showing no results
     let noResult = document.querySelector(".no-result");
     // Prevents page from reloading and sending data to a sever.
@@ -19,24 +19,45 @@
       method: "get"
     }).then(res => {
       res.json().then(result => {
-        if (result.total_count !== 0) {
-          list.style.display = "flex";
-          noResult.style.display = "none";
-          totalFound(result);
-          console.log(result.items);
-          result.items.forEach(item => {});
-        } else {
-          list.style.display = "none";
-          noResult.style.display = "flex";
-          alert(`No ${input} users found. Please try again!`);
-        }
+        result.total_count !== 0 ? addUsersToDOM(result) : noUsersFound();
       });
     });
 
+    function addUsersToDOM(users) {
+      userList.style.display = "flex";
+      noResult.style.display = "none";
+      totalFound(users.items);
+      console.log(users.items);
+      users.items.forEach(person => {
+        let detailedProfile = extractUserObject(person);
+        console.log("Detailed", detailedProfile);
+      });
+    }
+
+    function noUsersFound() {
+      numFound.style.display = "none";
+      userList.style.display = "none";
+      noResult.style.display = "flex";
+      alert(`No ${input} users found. Please try again!`);
+    }
+
+    function extractUserObject(user) {
+      let detailedData;
+      fetch(user.url, {
+        method: "get"
+      }).then(res => {
+        res.json().then(result => {
+          console.log(result);
+          detailedData = result;
+        });
+      });
+      return detailedData;
+    }
+
     function totalFound(users) {
       const numFound = document.querySelector("#users-found");
-      numFound.textContent = `Found ${users.total_count} results for ${input}`;
-      console.log(users);
+      numFound.style.display = "block";
+      numFound.textContent = `Found ${users.length} results for ${input}`;
     }
   });
 })();
