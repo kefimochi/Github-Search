41135040b6d0e3ddf31fa4ffad99412e9971ec87
Kate Efimova <kate.efimova0@gmail.com>
2019-08-07 09:51:41 -0700

    Now generates images & components

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,17 +1,21 @@
+let inputString = "";
+// Selects a container that shows a brief users desciption and
+// a total amount of users found.
+let userList = document.querySelector(".all-users-container");
+// Selects a page showing no results
+let noResult = document.querySelector(".no-result");
+// Prevents page from reloading and sending data to a sever.
+
+// Accepts string input, fetches data from GitHub's API
 (function() {
   let form = document.querySelector("#form");
 
   form.addEventListener("submit", function(e) {
     // Captures a string entered in the search field.
     let input = document.getElementById("search-bar").value;
+    inputString = input;
     // Adds searched value to GitHub's API
     let url = `https://api.github.com/search/users?q=${input}`;
-    // Selects a container that shows a brief users desciption and
-    // a total amount of users found.
-    let userList = document.querySelector(".all-users-container");
-    // Selects a page showing no results
-    let noResult = document.querySelector(".no-result");
-    // Prevents page from reloading and sending data to a sever.
     e.preventDefault();
 
     // Goes to a provided url and converts the received data into a JSON object
@@ -22,42 +26,54 @@
         result.total_count !== 0 ? addUsersToDOM(result) : noUsersFound();
       });
     });
-
-    function addUsersToDOM(users) {
-      userList.style.display = "flex";
-      noResult.style.display = "none";
-      totalFound(users.items);
-      console.log(users.items);
-      users.items.forEach(person => {
-        let detailedProfile = extractUserObject(person);
-        console.log("Detailed", detailedProfile);
-      });
-    }
-
-    function noUsersFound() {
-      numFound.style.display = "none";
-      userList.style.display = "none";
-      noResult.style.display = "flex";
-      alert(`No ${input} users found. Please try again!`);
-    }
-
-    function extractUserObject(user) {
-      let detailedData;
-      fetch(user.url, {
-        method: "get"
-      }).then(res => {
-        res.json().then(result => {
-          console.log(result);
-          detailedData = result;
-        });
-      });
-      return detailedData;
-    }
-
-    function totalFound(users) {
-      const numFound = document.querySelector("#users-found");
-      numFound.style.display = "block";
-      numFound.textContent = `Found ${users.length} results for ${input}`;
-    }
   });
 })();
+
+function addUsersToDOM(users) {
+  userList.style.display = "flex";
+  noResult.style.display = "none";
+  totalFound(users.items);
+  console.log(users.items);
+  users.items.forEach(person => {
+    extractUserObject(person);
+  });
+}
+
+// Resets everything to default if GitHub user is not found and
+// alerts about it.
+function noUsersFound() {
+  numFound.style.display = "none";
+  userList.style.display = "none";
+  noResult.style.display = "flex";
+  alert(`No ${inputString} users found. Please try again!`);
+}
+
+function extractUserObject(user) {
+  return fetch(user.url, {
+    method: "get"
+  }).then(res => {
+    res.json().then(result => {
+      userComponent(result);
+    });
+  });
+}
+
+function totalFound(users) {
+  const numFound = document.querySelector("#users-found");
+  numFound.style.display = "block";
+  numFound.textContent = `Found ${users.length} results for ${inputString}`;
+}
+
+function userComponent(user) {
+  console.log("user", user.avatar_url);
+  let div = document.createElement("div");
+  div.classList.add("simple-user");
+  let img = document.createElement("img");
+  img.setAttribute("src", user.avatar_url);
+  // let login = div.createElement("h3");
+  // login.textContent = "Kefiiiiiiiii";
+  userList.appendChild(div);
+  div.appendChild(img);
+}
+
+function detailedUserComponent() {}
